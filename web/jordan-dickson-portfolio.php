32e3aa11ea405eee6c1ee6f75917ace427bf5d5f
Jordan Dickson <jfdickson25@gmail.com>
2020-02-11 14:26:39 -0700

    asdfasd

--- web/jordan-dickson-portfolio.php
+++ web/jordan-dickson-portfolio.php
@@ -405,7 +405,7 @@
                   foreach ($db->query('SELECT courseCode FROM courses') AS $row) {
                     ?>
                         <tr>
-                          <td> <? echo $row['courseCode']; ?></td>
+                          <td> <? echo $row; ?></td>
                         </tr>
 
                     <?
