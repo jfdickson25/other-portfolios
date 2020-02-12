261cea03dcf824787765b37ea26ec1d6c98a3f13
Jordan Dickson <jfdickson25@gmail.com>
2020-02-12 00:41:33 -0700

    asdf

--- web/jordan-dickson-portfolio.php
+++ web/jordan-dickson-portfolio.php
@@ -191,7 +191,7 @@
         <div class="col-md-6 col-lg-4" style="text-align:center">
           <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
             <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-              <div class="portfolio-item-caption-content text-center text-white">
+              <div class="portfolio-item-caption-content text-center text-white" style="background-color:grey">
                 <i class="fas fa-plus fa-3x"></i>
               </div>
             </div>
