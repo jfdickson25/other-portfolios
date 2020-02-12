775c7037ea31bafaa5b3876684d0bea252162850
Jordan Dickson <jfdickson25@gmail.com>
2020-02-12 00:44:51 -0700

    work?

--- web/jordan-dickson-portfolio.php
+++ web/jordan-dickson-portfolio.php
@@ -190,7 +190,7 @@
         <!-- Portfolio Item 2 -->
         <div class="col-md-6 col-lg-4" style="text-align:center">
           <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
-            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100" style="background-color:grey; opacity:1">
+            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100" style="background-color:grey; opacity:.1">
               <div class="portfolio-item-caption-content text-center text-white">
                 <i class="fas fa-plus fa-3x"></i>
               </div>
