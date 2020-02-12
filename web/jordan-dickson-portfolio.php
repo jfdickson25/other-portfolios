f55212a9e9232d0d9186a0c8d8a10ea0d6d1e9dd
Jordan Dickson <jfdickson25@gmail.com>
2020-02-12 00:35:51 -0700

    Added new section

--- web/jordan-dickson-portfolio.php
+++ web/jordan-dickson-portfolio.php
@@ -166,6 +166,44 @@
     </div>
   </section>
 
+
+  <!-- Portfolio Section -->
+  <section class="page-section portfolio" id="portfolio">
+    <div class="container">
+
+      <!-- Portfolio Section Heading -->
+      <h2 class="page-section-heading text-center text-uppercase text-dark mb-0">Resume</h2>
+
+      <!-- Icon Divider -->
+      <div class="divider-custom">
+        <div class="divider-custom-line"></div>
+        <div class="divider-custom-icon">
+          <i class="fas fa-star"></i>
+        </div>
+        <div class="divider-custom-line"></div>
+      </div>
+
+      <!-- Portfolio Grid Items -->
+      <div class="row">
+
+
+        <!-- Portfolio Item 1 -->
+        <div class="col-md-6 col-lg-4">
+          <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+              <div class="portfolio-item-caption-content text-center text-white">
+                <i class="fas fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/portfolio/user.png" alt="" height="512px" width="512px">
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+
   <!-- Copyright Section -->
   <section class="copyright py-4 bg-light text-center text-dark">
     <div class="container">
