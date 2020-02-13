4196adb1b6437615d7099b7547e5753ae2d29da0
Jordan Dickson <jfdickson25@gmail.com>
2020-02-13 12:14:09 -0700

    Added text

--- web/jordan-dickson-portfolio.php
+++ web/jordan-dickson-portfolio.php
@@ -100,6 +100,7 @@
 
         <!-- Portfolio Item 2 -->
         <div class="col-md-6 col-lg-4">
+          <h3>About Me</h3>
           <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
             <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
               <div class="portfolio-item-caption-content text-center text-white">
