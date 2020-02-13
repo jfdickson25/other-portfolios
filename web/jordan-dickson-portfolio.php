43059bd12b77fd0e7875c30b4880e345432ea834
Jordan Dickson <jfdickson25@gmail.com>
2020-02-13 12:51:07 -0700

    Changed margin

--- web/jordan-dickson-portfolio.php
+++ web/jordan-dickson-portfolio.php
@@ -149,7 +149,7 @@
 
       <!-- About Section Content -->
       <div class="row">
-        <div class="col-lg-12 ml-6 mr-auto">
+        <div class="col-lg-12 ml-6">
           <ul>
             <li>I am a hard worker</li>
             <li>I bring an attitude of someone willing to learn</li>
